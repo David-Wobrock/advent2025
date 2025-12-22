e9b10ae6699fb59c7d9ff777ef78dd615634e9ca
David Wobrock <david.wobrock@backmarket.com>
2025-12-22 17:25:29 +0100

    Completed day 4, part 1.

---
+++ day4/part1.rs
@@ -0,0 +1,49 @@
+use std::fs;
+use std::collections::HashMap;
+
+#[derive(Eq, PartialEq, Hash, Clone)]
+struct TupleStruct{x: i32, y: i32}
+
+fn main() {
+    let mut num_rolls_accessible: u64 = 0;
+
+    let mut rolls: HashMap<TupleStruct, bool> = HashMap::new();
+    for (x, line) in fs::read_to_string("input").unwrap().lines().enumerate() {
+        for (y, c) in line.chars().enumerate() {
+            if c == '@' {
+                rolls.insert(TupleStruct{x: x as i32, y: y as i32}, true);
+            }
+        }
+    }
+
+    for roll_pos in rolls.keys() {
+        let x = roll_pos.x;
+        let y = roll_pos.y;
+        let mut num_neighbors = 0;
+        // Above
+        for i in -1..2 {
+            if rolls.contains_key(&TupleStruct{x: x-1, y: y+i}) {
+                num_neighbors += 1;
+            }
+        }
+        // Below
+        for i in -1..2 {
+            if rolls.contains_key(&TupleStruct{x: x+1, y: y+i}) {
+                num_neighbors += 1;
+            }
+        }
+        // Left & right
+        if rolls.contains_key(&TupleStruct{x: x, y: y-1}) {
+            num_neighbors += 1;
+        }
+        if rolls.contains_key(&TupleStruct{x: x, y: y+1}) {
+            num_neighbors += 1;
+        }
+
+        if num_neighbors < 4 {
+            num_rolls_accessible += 1;
+        }
+    }
+
+    println!("{}", num_rolls_accessible);
+}
